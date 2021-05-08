a2e7134bf31dc90735242c17796c9bb8b57acc75
EdersonDav <silva.edersonqueiroz@gmail.com>
2021-05-08 16:21:54 -0300

    Create global style

---
+++ src/style/global.ts
@@ -0,0 +1,22 @@
+import { createGlobalStyle } from 'styled-components';
+
+export default createGlobalStyle`
+  *{
+    margin: 0;
+    padding:0;
+    box-sizing: border-box;
+    outline: none;
+    text-decoration: none;
+    font-family: 'Poppins', sans-serif;
+  }
+  body{
+    background: #1B1B1F;
+    color: #EAEAEA;
+  }
+  button{
+    cursor: pointer;
+  }
+  h1{
+    font-weight: 500;
+  }
+`;
