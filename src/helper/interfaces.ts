d5c9028a6654f36ac43786f664404d116c068775
EdersonDav <silva.edersonqueiroz@gmail.com>
2021-05-15 18:11:14 -0300

    Adding movie interface

--- src/helper/interfaces.ts
+++ src/helper/interfaces.ts
@@ -4,6 +4,7 @@ export interface IMovies {
   adult: boolean;
   backdrop_path: string;
   genre_ids: number[];
+  genres: IGenres[];
   id: number;
   media_type: string;
   original_language: string;
@@ -25,4 +26,15 @@ export interface IProps {
 export interface IGenres {
   id: number;
   name: string;
+}
+
+export interface ITrailer {
+  id: string;
+  iso_639_1: string;
+  iso_3166_1: string;
+  key: string;
+  name: string;
+  site: string;
+  size: number;
+  type: string;
 }
